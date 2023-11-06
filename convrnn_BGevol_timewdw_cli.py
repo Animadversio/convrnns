6dd74851927d8c98d8d02ea74ba9f8700816c427
BinxuWang <binxu.wang@wustl.edu>
2023-11-06 14:54:47 -0600

    debug, add the reps

--- convrnn_BGevol_timewdw_cli.py
+++ convrnn_BGevol_timewdw_cli.py
@@ -115,13 +115,21 @@ parser.add_argument("--timewdws",
     help="time window counted in objective for running evolution, "
          "input as a list of tuples '(1, 16)', '(5,16)'"
 )
-cli_args = parser.parse_args(["--model_name", "rgc_intermediate", "--timewdws", "(1, 16)", "(5, 16)"])
+parser.add_argument(
+    "--reps",
+    type=int,
+    default=1,
+    help="Repetitions"
+)
+# cli_args = parser.parse_args(["--model_name", "rgc_intermediate", "--timewdws", "(1, 16)", "(5, 16)"])
+cli_args = parser.parse_args()
 cli_args = edict(vars(cli_args))
 # cli_args = edict(steps=100, chan_start=0, chan_end=2, optim=["CholCMA"], G="BigGAN", layername="conv9", model_name="rgc_intermediate")
 timewdws = cli_args.timewdws
 steps = cli_args.steps
 channel_range = (cli_args.chan_start, cli_args.chan_end)
 method_col = cli_args.optim
+reps = cli_args.reps
 args = edict({
     # "model_name": "rgc_intermediate", #'rgc_intermediate',  # Required argument, so no default value is effectively used
     # "out_layers": 'conv2',
@@ -199,81 +207,82 @@ for chan_id in range(channel_range[0], channel_range[1]):
                                                                         idxtuple[1] - idxtuple[0]))
         time_wdw_str = "T%02d-T%02d" % (timewdw_proc[0], timewdw_proc[1])
         time_slice = slice(idxtuple[0], idxtuple[1])
-        if args.G == "BigGAN":
-            fixnoise = 0.7 * truncated_noise_sample(1, 128)
-            init_code = np.concatenate((fixnoise, np.zeros((1, 128))), axis=1)
-        elif args.G == "fc6":
-            init_code = np.random.randn(1, 4096)
-        else:
-            raise NotImplemented("Not recognized G type.")
+        for rep in range(reps):
+            if args.G == "BigGAN":
+                fixnoise = 0.7 * truncated_noise_sample(1, 128)
+                init_code = np.concatenate((fixnoise, np.zeros((1, 128))), axis=1)
+            elif args.G == "fc6":
+                init_code = np.random.randn(1, 4096)
+            else:
+                raise NotImplemented("Not recognized G type.")
 
-        new_codes = init_code
-        optimizer_col = [label2optimizer(methodlabel, init_code, args.G, Hdata=None) for methodlabel in method_col]
-        RND = np.random.randint(1E5)
-        for methodlab, optimizer in zip(method_col, optimizer_col):
-            if args.G == "fc6":
-                methodlab += "-fc6"
-            elif args.G == "BigGAN":
-                methodlab += "-BigGAN"
-            # main loop of Evolution.
-            savedict = dict({"scores": [], "scores_dyn": [], "generations": [], "best_codes": []}) # "mean_codes": [],
-            best_imgs = []
-            for block_i in range(steps):
-                n_imgs = len(new_codes)
-                latent_code = torch.from_numpy(np.array(new_codes)).float()
-                # savedict["mean_codes"].append(new_codes.mean(axis=0, keepdims=True))
-                imgs = G.visualize(latent_code.cuda()).detach().cpu()
-                # imgs = G.visualize(latent_code).detach()
-                imgs_pp = F.interpolate(imgs, size=(224, 224), mode='bilinear', align_corners=False)
-                imgs_pp_np = imgs_pp.numpy().transpose(0, 2, 3, 1)
-                imgs_pp_np = normalize_ims(imgs_pp_np)
-                # zero pad to batch size
-                scores_dyn = []
-                scores = []
-                for n_csr in range(0, n_imgs, num_batch):
-                    n_end = min(n_imgs, n_csr + num_batch)
-                    if n_end - n_csr < num_batch:
-                        imgs_pp_batch = np.concatenate([imgs_pp_np[n_csr:n_end, :], np.zeros((num_batch - (n_end - n_csr), 224, 224, 3))], axis=0)
-                    else:
-                        imgs_pp_batch = imgs_pp_np[n_csr:n_end, :]
-                    # Record activations
-                    # scores = scorer.score_tsr(imgs)
-                    y_eval = sess.run(y, feed_dict={inputs: imgs_pp_batch})
-                    # Calculate scores
-                    time_pnts = list(y_eval[layername].keys()) # [Tpnts,]
-                    acttsr = np.array([*y_eval[layername].values()]) # [Tpnts, batch, H, W, C, ]
-                    Hcent, Wcent = acttsr.shape[2] // 2, acttsr.shape[3] // 2
-                    scores_dyn_batch = acttsr[:, :(n_end - n_csr), Hcent, Wcent, chan_id]  # [T, n_imgs,]
-                    scores_batch = scores_dyn_batch[time_slice, :].mean(axis=0)  # [n_imgs,]
-                    scores_dyn.append(scores_dyn_batch)
-                    scores.append(scores_batch)
+            new_codes = init_code
+            optimizer_col = [label2optimizer(methodlabel, init_code, args.G, Hdata=None) for methodlabel in method_col]
+            RND = np.random.randint(1E5)
+            for methodlab, optimizer in zip(method_col, optimizer_col):
+                if args.G == "fc6":
+                    methodlab += "-fc6"
+                elif args.G == "BigGAN":
+                    methodlab += "-BigGAN"
+                # main loop of Evolution.
+                savedict = dict({"scores": [], "scores_dyn": [], "generations": [], "best_codes": []}) # "mean_codes": [],
+                best_imgs = []
+                for block_i in range(steps):
+                    n_imgs = len(new_codes)
+                    latent_code = torch.from_numpy(np.array(new_codes)).float()
+                    # savedict["mean_codes"].append(new_codes.mean(axis=0, keepdims=True))
+                    imgs = G.visualize(latent_code.cuda()).detach().cpu()
+                    # imgs = G.visualize(latent_code).detach()
+                    imgs_pp = F.interpolate(imgs, size=(224, 224), mode='bilinear', align_corners=False)
+                    imgs_pp_np = imgs_pp.numpy().transpose(0, 2, 3, 1)
+                    imgs_pp_np = normalize_ims(imgs_pp_np)
+                    # zero pad to batch size
+                    scores_dyn = []
+                    scores = []
+                    for n_csr in range(0, n_imgs, num_batch):
+                        n_end = min(n_imgs, n_csr + num_batch)
+                        if n_end - n_csr < num_batch:
+                            imgs_pp_batch = np.concatenate([imgs_pp_np[n_csr:n_end, :], np.zeros((num_batch - (n_end - n_csr), 224, 224, 3))], axis=0)
+                        else:
+                            imgs_pp_batch = imgs_pp_np[n_csr:n_end, :]
+                        # Record activations
+                        # scores = scorer.score_tsr(imgs)
+                        y_eval = sess.run(y, feed_dict={inputs: imgs_pp_batch})
+                        # Calculate scores
+                        time_pnts = list(y_eval[layername].keys()) # [Tpnts,]
+                        acttsr = np.array([*y_eval[layername].values()]) # [Tpnts, batch, H, W, C, ]
+                        Hcent, Wcent = acttsr.shape[2] // 2, acttsr.shape[3] // 2
+                        scores_dyn_batch = acttsr[:, :(n_end - n_csr), Hcent, Wcent, chan_id]  # [T, n_imgs,]
+                        scores_batch = scores_dyn_batch[time_slice, :].mean(axis=0)  # [n_imgs,]
+                        scores_dyn.append(scores_dyn_batch)
+                        scores.append(scores_batch)
 
-                scores = np.concatenate(scores, axis=0)
-                scores_dyn = np.concatenate(scores_dyn, axis=1)
-                # Perform CMA updates
-                new_codes = optimizer.step_simple(scores, new_codes, )
-                print("step %d score %.3f (%.3f) (norm %.2f )" % (
-                        block_i, scores.mean(), scores.std(), latent_code.norm(dim=1).mean(),))
-                savedict["scores"].append(scores)
-                savedict["scores_dyn"].append(scores_dyn.transpose(1, 0)) # batch, time
-                savedict["generations"].append([block_i]*n_imgs)
-                best_imgs.append(imgs[np.argmax(scores), :, :, :])
-                savedict["best_codes"].append(new_codes[np.argmax(scores), :][np.newaxis])
-                torch.cuda.empty_cache()
+                    scores = np.concatenate(scores, axis=0)
+                    scores_dyn = np.concatenate(scores_dyn, axis=1)
+                    # Perform CMA updates
+                    new_codes = optimizer.step_simple(scores, new_codes, )
+                    print("step %d score %.3f (%.3f) (norm %.2f )" % (
+                            block_i, scores.mean(), scores.std(), latent_code.norm(dim=1).mean(),))
+                    savedict["scores"].append(scores)
+                    savedict["scores_dyn"].append(scores_dyn.transpose(1, 0)) # batch, time
+                    savedict["generations"].append([block_i]*n_imgs)
+                    best_imgs.append(imgs[np.argmax(scores), :, :, :])
+                    savedict["best_codes"].append(new_codes[np.argmax(scores), :][np.newaxis])
+                    torch.cuda.empty_cache()
 
-            for k in savedict:
-                savedict[k] = np.concatenate(savedict[k], axis=0)
-            savedict["time_pnts"] = time_pnts
-            savedict["Hcent"] = Hcent
-            savedict["Wcent"] = Wcent
-            plt.imsave(join(expdir, f"{layername}_chan{chan_id:03d}_{time_wdw_str}_lastgen_{methodlab}_{RND}.png"),
-                       imgs.permute(0, 2, 3, 1).numpy()[0], )
-            np.savez(join(expdir, f"scores_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.npz"),
-                     **savedict)
-            mtg_exp = ToPILImage()(make_grid(best_imgs, nrow=10))
-            mtg_exp.save(join(expdir, f"besteachgen_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.jpg" ))
-            mtg = ToPILImage()(make_grid(imgs, nrow=7))
-            mtg.save(join(expdir, f"lastgen_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.jpg"))
+                for k in savedict:
+                    savedict[k] = np.concatenate(savedict[k], axis=0)
+                savedict["time_pnts"] = time_pnts
+                savedict["Hcent"] = Hcent
+                savedict["Wcent"] = Wcent
+                plt.imsave(join(expdir, f"{layername}_chan{chan_id:03d}_{time_wdw_str}_lastgen_{methodlab}_{RND}.png"),
+                           imgs.permute(0, 2, 3, 1).numpy()[0], )
+                np.savez(join(expdir, f"scores_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.npz"),
+                         **savedict)
+                mtg_exp = ToPILImage()(make_grid(best_imgs, nrow=10))
+                mtg_exp.save(join(expdir, f"besteachgen_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.jpg" ))
+                mtg = ToPILImage()(make_grid(imgs, nrow=7))
+                mtg.save(join(expdir, f"lastgen_{layername}_chan{chan_id:03d}_{time_wdw_str}_{methodlab}_{RND}.jpg"))
 
 #%%
 sess.close()
