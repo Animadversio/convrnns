c877adac972bc20230b33f5382a283c10bd3b791
BinxuWang <binxu.wang@wustl.edu>
2023-11-07 01:40:15 -0600

    add time window psth analysis

--- analysis/evol_wdw_analysis.py
+++ analysis/evol_wdw_analysis.py
@@ -7,6 +7,7 @@ import pandas as pd
 from pathlib import Path
 from os.path import join
 import seaborn as sns
+from tqdm import trange
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from circuit_toolkit.plot_utils import make_grid, make_grid_T, make_grid_np, show_image_without_frame, saveallforms
@@ -40,50 +41,55 @@ def extract_score_stats(score_data):
 
 
 #%%
+plt.switch_backend("agg")  # 'module://backend_interagg'
+figdir = r"F:\insilico_exps\convrnn_Evol\fasrc_runs\summary\BGFC_timewdw_psth_cmp"
+os.makedirs(figdir, exist_ok=True)
 model_name = "rgc_intermediate"
 # for layernum in range(4, 10+1):
-layernum = 8
-layername = f"conv{layernum}"
-layerdir = rf"F:\insilico_exps\convrnn_Evol\fasrc_runs\rgc_intermediate-{layername}_timewdw"
-T_active = layernum  # start receiving input at this timepoint
-for channum in range(40):
-    # for channum in range(50):
-    score_traj_mat = defaultdict(list)
-    score_traj_dyn_mat = defaultdict(list)
-    max_score_mat = defaultdict(list)
-    max_score_dyn_mat = defaultdict(list)
-    final_score_mat = defaultdict(list)
-    final_score_dyn_mat = defaultdict(list)
-    # for T in range(T_active, 16 + 1):
-    # parse these type of patterns
-    BG_fns = list(Path(layerdir).glob(rf"scores_{layername}_chan{channum:03d}_T{T_active:02d}-T16_CholCMA-BigGAN_*.npz"))
-    FC_fns = list(Path(layerdir).glob(rf"scores_{layername}_chan{channum:03d}_T{T_active:02d}-T16_CholCMA-fc6_*.npz"))
-    try:
-        assert len(list(BG_fns)) == 5
-        assert len(list(FC_fns)) == 5
-    except AssertionError:
-        print(f"Missing files for {layername} chan{channum} T{T_active}-T16 in either BG or FC")
+for layernum in trange(1, 10+1):
+    layername = f"conv{layernum}"
+    layerdir = rf"F:\insilico_exps\convrnn_Evol\fasrc_runs\rgc_intermediate-{layername}_timewdw"
+    T_active = layernum  # start receiving input at this timepoint
+    for channum in trange(50):
+        # for channum in range(50):
+        score_traj_mat = defaultdict(list)
+        score_traj_dyn_mat = defaultdict(list)
+        max_score_mat = defaultdict(list)
+        max_score_dyn_mat = defaultdict(list)
+        final_score_mat = defaultdict(list)
+        final_score_dyn_mat = defaultdict(list)
+        # for T in range(T_active, 16 + 1):
+        # parse these type of patterns
+        BG_fns = list(Path(layerdir).glob(rf"scores_{layername}_chan{channum:03d}_T{T_active:02d}-T16_CholCMA-BigGAN_*.npz"))
+        FC_fns = list(Path(layerdir).glob(rf"scores_{layername}_chan{channum:03d}_T{T_active:02d}-T16_CholCMA-fc6_*.npz"))
+        try:
+            assert len(list(BG_fns)) == 5
+            assert len(list(FC_fns)) == 5
+        except AssertionError:
+            print(f"Missing files for {layername} chan{channum} T{T_active}-T16 in either BG or FC")
 
-    final_score_dyn_col = defaultdict(list)
-    for repi in range(5):
-        BGdata = np.load(BG_fns[repi], allow_pickle=True)
-        FCdata = np.load(FC_fns[repi], allow_pickle=True)
-        score_dict_BG = extract_score_stats(BGdata)
-        score_dict_FC = extract_score_stats(FCdata)
-        final_score_dyn_col["BigGAN"].append(score_dict_BG["final_score_dyn"])
-        final_score_dyn_col["DeePSim"].append(score_dict_FC["final_score_dyn"])
-    for label in final_score_dyn_col:
-        final_score_dyn_col[label] = np.array(final_score_dyn_col[label])
+        final_score_dyn_col = defaultdict(list)
+        for repi in range(5):
+            BGdata = np.load(BG_fns[repi], allow_pickle=True)
+            FCdata = np.load(FC_fns[repi], allow_pickle=True)
+            score_dict_BG = extract_score_stats(BGdata)
+            score_dict_FC = extract_score_stats(FCdata)
+            final_score_dyn_col["BigGAN"].append(score_dict_BG["final_score_dyn"])
+            final_score_dyn_col["DeePSim"].append(score_dict_FC["final_score_dyn"])
+        for label in final_score_dyn_col:
+            final_score_dyn_col[label] = np.array(final_score_dyn_col[label])
 
-    plt.plot(range(T_active, 16+1), final_score_dyn_col["DeePSim"].T, label="DeePSim", color="blue")
-    plt.plot(range(T_active, 16+1), final_score_dyn_col["BigGAN"].T, label="BigGAN", color="red")
-    plt.legend()
-    plt.title(f"Layer {layernum} Channel {channum}")
-    plt.xlabel("Time steps (10ms)")
-    plt.ylabel("Activation")
-    # plt.savefig(join(layerdir, f"Layer{layernum}_chan{channum}_finalscoredyn.png"))
-    plt.show()
-
+        plt.subplots(1, 1, figsize=(5.5, 4.5))
+        plt.plot(range(T_active, 16+1), final_score_dyn_col["DeePSim"].T, label="DeePSim", color="blue")
+        plt.plot(range(T_active, 16+1), final_score_dyn_col["BigGAN"].T, label="BigGAN", color="red")
+        plt.legend()
+        plt.title(f"{model_name} Layer {layernum} Channel {channum},  Objective T{T_active}-T16")
+        plt.xlabel("Time steps (10ms)")
+        plt.ylabel("Activation")
+        saveallforms(figdir, f"{model_name}_{layernum}_chan{channum}_T{T_active}-T16_obj_final_psth_cmp")
+        # plt.savefig(join(layerdir, f"Layer{layernum}_chan{channum}_finalscoredyn.png"))
+        plt.show()
+        plt.close("all")
 #%%
 
 #%%
